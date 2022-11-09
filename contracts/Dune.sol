e3bb439cf89dea28d28655bee216d56c23c5223f
julienbrg <julien@strat.cc>
2022-11-09 11:15:05 +0100

    rm security contact

--- contracts/Dune.sol
+++ contracts/Dune.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @custom:security-contact julien@strat.cc
 contract Dune is ERC20 {
     constructor() ERC20("Dune", "DUNE") {}
 
